607488dd3c8f55a068f4c43130e62bc444944f06
1213leejs <1213leejs@gmail.com>
2024-12-10 21:47:04 +0900

    reformed simulation

--- game/game_logic.py
+++ game/game_logic.py
@@ -1,5 +1,4 @@
 import random
-from tracemalloc import start
 import numpy as np
 from numpy import number
 
@@ -25,6 +24,7 @@ transformations = [
     [8, 5, 2, 7, 4, 1, 6, 3, 0],  # 반시계방향 90도 > 좌우 대칭
 ]
 
+start_board = [""]*9
 # boards = [[""]*9 for _ in range(3**9)] << 모든 보드 구성 만들어놓기?
 dp_state = [""] * (3**9)
 dp_prob = [[""]*9] * (3**9)
@@ -80,7 +80,7 @@ def get_dp(game_board, current_turn):
     
     opposite_turn = "O" if current_turn == "X" else "X"
     
-    if game_board != [""]*9: # 초기상태 예외처리
+    if game_board != start_board: # 초기상태 예외처리
         if check_winner(game_board, opposite_turn): # 패배
             dp_state[index] = opposite_turn
             return opposite_turn
@@ -107,7 +107,6 @@ def get_dp(game_board, current_turn):
     
     return result
     
-start_board = [""]*9
 get_dp(start_board, 'X')
 
 def computer_move(game_board, current_turn):
@@ -126,7 +125,7 @@ def computer_move(game_board, current_turn):
 game_board: 문자열 9개 리스트. 현재 게임 상황 표시
 current_turn: 그 다음 차례 ('O' | 'X')
 '''
-def simulation(game_board, current_turn, initial_depth=7, move_weights=(20, 5, 1)): 
+def simulation(game_board, current_turn, move_weights=(20, 10, 1)): 
     comment = "comment"
     
     initial_index = board_index(game_board)
@@ -136,52 +135,52 @@ def simulation(game_board, current_turn, initial_depth=7, move_weights=(20, 5, 1
     simulation_board = game_board.copy()
     opposite_turn = "O" if current_turn == "X" else "X"
     
-    def simulate(game_board, turn, depth, cell_index):
-        index = board_index(game_board)
-        if "" not in dp_prob[index] and type(dp_prob[index][cell_index]) == float:
-            return dp_prob[index][cell_index]
+    def simulate(prev_game_board, prev_board_index, turn, cell_index):
+        if "" not in dp_prob[prev_board_index] and type(dp_prob[prev_board_index][cell_index]) == float:
+            return dp_prob[prev_board_index][cell_index]
         
-        for index in transformation_indices(game_board):
-            dp_prob[index] = game_board.copy()
+        prev_game_board[cell_index] = turn
+        if check_winner(prev_game_board, turn):
+            return 1
+        if "" not in prev_game_board:
+            return 0.5
         
-        if check_winner(game_board, turn):
-            return 1 if turn == current_turn else 0
-            
-        state = get_dp(game_board, turn)
-        if depth == 0 or "" not in game_board:
-            return 1 if state == current_turn else 0 if state == opposite_turn else 0.5
+        cur_game_board = prev_game_board.copy()
+        prev_game_board[cell_index] = ""
+        cur_board_index = board_index(cur_game_board)
+        dp_prob[cur_board_index] = cur_game_board.copy()
         
         next_turn = "O" if turn == "X" else "X"
         values = []
-        for i, cell in enumerate(game_board):
+        for i, cell in enumerate(cur_game_board):
             if cell == "":
-                game_board[i] = turn
-                expected_value = simulate(game_board, next_turn, depth-1, i)
+                expected_value = simulate(cur_game_board, cur_board_index, next_turn, i)
                 values.append(expected_value)
-                for j, index in enumerate(transformation_indices(game_board)):
-                    dp_prob[index][transformations[j][i]] = expected_value
-                game_board[i] = ""
-                
+                dp_prob[cur_board_index][i] = expected_value
+        
+        for transform in transformations:
+            transformed_index = board_index([cur_game_board[i] for i in transform])
+            dp_prob[transformed_index] = [dp_prob[cur_board_index][i] for i in transform]
+        
         weights = [
-            move_weights[0] if value == 1 else move_weights[1] if value == 0.5
+            move_weights[0] if value == 1
+            else move_weights[1] if value == 0.5
             else move_weights[2] for value in values
         ]
         
         weights_sum = sum(weights)
-        return 0 if 0 in values and depth == initial_depth else sum(value * weight / weights_sum for value, weight in zip(values, weights))
+        return round(1 - sum(value * weight / weights_sum for value, weight in zip(values, weights)), 2)
     
     for i in range(len(game_board)):
         if game_board[i] == "":
-            game_board[i] = current_turn
-            simulation_board[i] = (round(simulate(game_board, opposite_turn, initial_depth, i), 2))
-            game_board[i] = ""
+            simulation_board[i] = simulate(game_board, initial_index, current_turn, i)
     
-    for index in transformation_indices(game_board):
-        dp_prob[index] = simulation_board
-    
-    return simulation_board, comment# game_board + ��????��? ��??��?? ��?����??(?????��??) / ��??��??��?? ��??��??��? ??��?? ??��??��??
-    
-              
+    for transform in transformations:
+        transformed_index = board_index([game_board[i] for i in transform])
+        dp_prob[transformed_index] = [simulation_board[i] for i in transform]
+        
+    return simulation_board, comment # game_board + ��????��? ��??��?? ��?����??(?????��??) / ��??��??��?? ��??��??��? ??��?? ??��??��??
+
     # board_size = len(game_board)
     #  # 승/무/패 가중치
     # for i in range(board_size):
